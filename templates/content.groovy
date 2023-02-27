dd8c90d0b7a0b64f8c6c2eed77890aa5e2fc2e03
panduit-psh <Paul.Hofferkamp@panduit.com>
2023-02-27 15:14:05 -0600

    Update content.groovy

--- templates/content.groovy
+++ templates/content.groovy
@@ -7,9 +7,9 @@ void renderPage(Object pageData, GPathResult inXml, MarkupBuilder outXml, Map re
     def commons = shell.parse(new File('templates/.commons.groovy').text)
 
     def pageProperties = commons.pageProperties(pageData, inXml, '/apps/panduit/templates/basedetailpageblog','panduit/components/page/basedetailpageblog', replacements)
-    pageProperties['cq:tags'] = ''
-    pageProperties['blogAuthor'] = ''
-    pageProperties['blogCategories'] = ''
+    // pageProperties['cq:tags'] = ''
+    // pageProperties['blogAuthor'] = ''
+    // pageProperties['blogCategories'] = ''
 
     Date blogCreationDateOverride = new Date(inXml.pubDate.toString())
     pageProperties['blogCreationDateOverride'] = '{Date}' + blogCreationDateOverride.format("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
@@ -17,7 +17,7 @@ void renderPage(Object pageData, GPathResult inXml, MarkupBuilder outXml, Map re
     outXml.'jcr:root'(commons.rootProperties()) {
         'jcr:content'(pageProperties) {
             'blogcontent'(commons.component('foundation/components/parsys')){
-                'text'(commons.component('panduit/components/content/general/text', ['textIsRich': true, 'text': inXml.encoded.toString()]))
+                'text'(commons.component('panduit/components/content/general/text', ['textIsRich': true, 'text': inXml.encoded]))
             }
         }
     }
